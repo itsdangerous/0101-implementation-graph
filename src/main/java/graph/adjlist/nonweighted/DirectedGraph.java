af754ad1c4226fd25ce8a8c8b00a202fd8ad46be
itsdangerous <danger121@naver.com>
2023-02-01 01:20:40 +0900

    impl: 가중치 없는 방향그래프 - 인접리스트

--- src/main/java/graph/adjlist/nonweighted/DirectedGraph.java
+++ src/main/java/graph/adjlist/nonweighted/DirectedGraph.java
@@ -6,11 +6,17 @@ public class DirectedGraph {
     
     public DirectedGraph(int size) {
     	//TODO : 생성자 구현 -> adjList 초기화
-    	
+        adjList = new ArrayList[size+1]; // 리스트 선언
+
+        for (int i = 0; i < size + 1; i++) {
+            adjList[i] = new ArrayList<>(); // 2차원 배열 생성 및 초기화
+        }
     }
+
     
     public void addEdge(int from, int to) {
     	//TODO : 간선 추가 메서드 구현
+        adjList[from].add(to); // 간선 추가
     	
     }
 }
