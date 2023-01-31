1950874b31e17baa22b89ee0907a2ea6febd267c
itsdangerous <danger121@naver.com>
2023-02-01 01:21:38 +0900

    impl: 가중치 있는 방향그래프 - 인접리스트

--- src/main/java/graph/adjlist/weighted/DirectedGraph.java
+++ src/main/java/graph/adjlist/weighted/DirectedGraph.java
@@ -1,4 +1,6 @@
 package graph.adjlist.weighted;
+import org.w3c.dom.Node;
+
 import java.util.ArrayList;
 
 public class DirectedGraph {
@@ -6,11 +8,16 @@ public class DirectedGraph {
     
     public DirectedGraph(int size) {
     	//TODO : 생성자 구현 -> adjList 초기화
+        adjList = new ArrayList[size+1];
+        for (int i = 0; i < size + 1; i++) {
+            adjList[i] = new ArrayList<>();
+        }
      
     }
-    
     public void addEdge(int from, int to, int weight) {
     	//TODO : 간선 추가 메서드 구현
+        adjList[from].add(new Node(to, weight)); // nonweighted와 달리, Node를 추가해줌. 노드는 고유 노드넘버와 가중치를 갖고있음
+
      
     }
     
@@ -19,9 +26,13 @@ public class DirectedGraph {
     	int nodeNumber;
         int weight;
     	
-    	public Node() {
+    	public Node() { // default
     		// TODO: 생성자 만들기 -> 파라미터 추가 필요!
-      
     	}
+
+        public Node(int nodeNumber, int weight) {
+            this.nodeNumber = nodeNumber;
+            this.weight = weight;
+        }
     }
 }
