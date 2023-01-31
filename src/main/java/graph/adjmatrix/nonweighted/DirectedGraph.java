178cfc3c356ec16a915a2873ae541782f029efd1
itsdangerous <danger121@naver.com>
2023-02-01 01:03:37 +0900

    impl: 가중치 있는 무방향그래프 - 인접행렬

--- src/main/java/graph/adjmatrix/nonweighted/DirectedGraph.java
+++ src/main/java/graph/adjmatrix/nonweighted/DirectedGraph.java
@@ -5,11 +5,11 @@ public class DirectedGraph {
     
     public DirectedGraph(int size) {
     	//TODO : 생성자 구현 -> adjMatrix 초기화
-    	
+        adjMatrix = new int[size + 1][size + 1];
     }
     
     public void addEdge(int from, int to) {
     	//TODO : 간선 추가 메서드 구현
-    	
+        adjMatrix[from][to] = 1;
     }
 }
--- src/main/java/graph/adjmatrix/weighted/UndirectedGraph.java
+++ src/main/java/graph/adjmatrix/weighted/UndirectedGraph.java
@@ -5,11 +5,12 @@ public class UndirectedGraph {
     
     public UndirectedGraph(int size) {
     	//TODO : 생성자 구현 -> adjMatrix 초기화
-    	
+        adjMatrix = new int[size+1][size+1];
     }
     
     public void addEdge(int v1, int v2, int weight) {
     	//TODO : 간선 추가 메서드 구현
-    	
+        adjMatrix[v1][v2] = weight;
+        adjMatrix[v2][v1] = weight;
     }
 }
